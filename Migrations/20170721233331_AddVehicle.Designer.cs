33d666867644e5cd1bd5d93629efd2ca86665b03
Jan <jasiu@360.st>
2017-07-22 01:38:55 +0200

    Added Vehicle to dbcontext and created new dbset for vehiclefeatures.

--- Migrations/20170721233331_AddVehicle.Designer.cs
+++ Migrations/20170721233331_AddVehicle.Designer.cs
@@ -8,9 +8,10 @@ using Vega.Persistence;
 namespace Vega.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170721233331_AddVehicle")]
+    partial class AddVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -5,13 +5,21 @@ namespace Vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Feature> Features { get; set; }
+
+        public DbSet<Vehicle> Vehicles {get;set;}
+
         public VegaDbContext(DbContextOptions<VegaDbContext> option)
         : base(option)
         {
 
         }
 
-        public DbSet<Make> Makes { get; set; }
-        public DbSet<Feature> Features { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeatures>().HasKey(vf => 
+                new { vf.VehicleId, vf.FeatureId});
+        }
     }
 }
